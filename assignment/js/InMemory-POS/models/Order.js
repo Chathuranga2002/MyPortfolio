f0a9857f8195a93827442e082febe045de1b6ef5
Chathuranga2002 <121780050+Chathuranga2002@users.noreply.github.com>
2023-06-21 11:22:22 +0530

    inMemory update 2

---
+++ assignment/js/InMemory-POS/models/Order.js
@@ -0,0 +1,9 @@
+order={
+    oId : "",
+    cusId : "",
+    date : "",
+    orderDetails : undefined,
+    total : 0,
+    subTotal : 0,
+    discount : 0
+}
---
+++ assignment/js/InMemory-POS/models/OrderDetails.js
@@ -0,0 +1,8 @@
+orderDetails={
+        iCode : "",
+        itemName : "",
+        price : 0,
+        Qty : 0,
+        total : 0
+
+}
