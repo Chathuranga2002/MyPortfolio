86c39ed97f8beed2f64a66d152bd30f7dd6ce4cf
Chathuranga2002 <121780050+Chathuranga2002@users.noreply.github.com>
2023-06-27 11:23:58 +0530

    assigment update

---
+++ assets/other/js.js
@@ -0,0 +1,9 @@
+
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Full Stack Developer","Web Designer" , "QA Engineer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
